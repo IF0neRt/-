a24c5f8c3910c55fb186804d5ac944f181c75455
SnipGhost <snipghost@list.ru>
2017-06-19 19:05:52 +0300

    Remake functions

--- Om.c
+++ Om.c
@@ -1,90 +1,81 @@
 #include <stdio.h>
 
 int read_word(char s[], int *i, char w[])
-
-  int y;
-
 {
-	do
-	{
-	  
-	  *i = *i + 1;
+    int y;
 
-	}while (s[i] <= ' ') && (s[i] != '\0');
+    do
+    {
+      
+      *i = *i + 1;
 
-	y = 0;
+    } while ((s[i] <= ' ') && (s[i] != '\0'));
 
-	do
-	{
-		w[y] = s[*i];
-		y = y + 1;
-		*i = *i + 1;
-	}while (s[*i] > ' ');
+    y = 0;
 
-	w[y] = '\0';
+    do
+    {
+        w[y] = s[*i];
+        y = y + 1;
+        *i = *i + 1;
+    } while (s[*i] > ' ');
 
-	return y;
+    w[y] = '\0';
+
+    return y;
 }
 
 
 int palindrom(char s, int l)
-
-int i, y;
-
 {
+  int i, y;
   i = 0;
   y = l - 1;
 
   do
   {
 
-  	i = i + 1;
-  	y = y - 1;
+    i = i + 1;
+    y = y - 1;
 
-  }while (i < y) && (s[i] = s[y]);
+  } while (i < y) && (s[i] = s[y]);
 
-return (i >= y);
+  return (i >= y);
 }
 
 
 int del_palindrom(char s);
-int i, z, l;
-
 {
-	i = 0;
-	z = 0;
+    int i, z, l;
+    i = 0;
+    z = 0;
  
-	do
-	{
-  		l = read_word(s, &i, w);
-  		 if (palindrom(w,l) = 0)
-  		 {
-  		 	y = 0;
-  		 	do
-  		 	{
-  		 		s[z] = w[y];
-  		 		z = z + 1;
-  		 		y = y + 1;
-  		 	} while (w[y] != '\0');
-  		 	s[z++] = ' ';
-  		 } 
-	}while (s[i] != '\0');
+    do
+    {
+        l = read_word(s, &i, w);
+         if (palindrom(w,l) = 0)
+         {
+            y = 0;
+            do
+            {
+                s[z] = w[y];
+                z = z + 1;
+                y = y + 1;
+            } while (w[y] != '\0');
+            s[z++] = ' ';
+         } 
+    } while (s[i] != '\0');
 
-	s[z] = '\0';
+    s[z] = '\0';
 
 }
 
 int main()
+{
+    
+    return 0;
+}
 //ввод строки(s)
 // del_polindrom(s);// Вызов функции удаления слова полиндром
 // Вывод строки(s)
 // Конец
-
-
-
-
-
-
-
-
-
